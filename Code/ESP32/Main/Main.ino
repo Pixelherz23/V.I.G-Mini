0a2946292dd70403b1c4a3561d19c0ef5cc14c67
Pixelherz23 <zocker34298@gmail.com>
2021-05-21 16:56:16 +0200

    WiFi init

--- Code/ESP32/Main/Main.ino
+++ Code/ESP32/Main/Main.ino
@@ -1,78 +1,67 @@
-#include "DHTesp.h"
-<<<<<<< Updated upstream
-
-DHTesp dht;
-int DHT11_PIN =  21;
-int MOIS_SENSOR_PIN = 32;/
-int FAN_PIN = 17;
-
-void setup(){
-  
-dht.setup(DHT11_PIN, DHTesp::DHT11);
-pinMode(FAN_PIN, OUTPUT); 
- Serial.begin(9600);
-}
-
-void loop(){
- //Serial.println(getTempAndHum(2000));
-  //Serial.println(getMoisture(1000));
-  digitalWrite(FAN_PIN, HIGH); 
-  delay(10000);
-  digitalWrite(FAN_PIN, LOW); 
-  delay(10000);
- 
-}
-/*
- * @delay als gerade Zahl angeben
- */
-
- int getMoisture(int del){
-  
-   int anRead= analogRead(MOIS_SENSOR_PIN);
-   delay(del);
-   int erg = map(anRead,4095,0,0,100);//4095=>0% trocken, 0=> 100% nass
-   return erg;
- }
-String getTempAndHum(int delayNum){
-  delayNum = delayNum/2;
-  
-  TempAndHumidity lastValues = dht.getTempAndHumidity();
-  delay(1000); //zweitePause weil ansonsen zu schnell
-  String erg= "Temp: "+String(lastValues.temperature,0)+",Hum: "+String(lastValues.humidity,0);
-  delay(1000);
-
-  return erg;
-  
-  }
-  
-=======
-#include "WiFi.h"
+#include <DHTesp.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
 DHTesp dht;
 const int DHT11_PIN =  21;
 const int MOIS_SENSOR_PIN = 32;
 const int FAN_PIN = 17;
 
-const String ssid = "yourNetworkName";
-const String password =  "yourNetworkPass";
+const String SSID_D = "yourNetworkName";
+const String PASSWORD =  "yourNetworkPass";
+const String SERVER_URL = "";
 
+HTTPClient http;
 
-int tempLimit = 15;
+int temp;
+int mois;
+int hum;
+
+int tempLimit = 24;
 boolean isTempControlOn = true;
 
 
 void setup() {
 
+  
+  //WiFi.mode(WIFI_STA);
+  //WiFi.begin(SSID_D, PASSWORD);
+/*
+  while(WiFi.status()!= WL_CONNECTED){
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+    
+  }
+  */
   dht.setup(DHT11_PIN, DHTesp::DHT11);
   pinMode(FAN_PIN, OUTPUT);
   Serial.begin(9600);
-  //WiFi.begin(ssid, password);
+    
 }
 
+
 void loop() {
-  Serial.println(getTempAndHum());
-  delay(1000);
+
+  /*
+  if(WiFi.status()== WL_CONNECTED){
+    
+  }else{
+     while(WiFi.status()!= WL_CONNECTED){
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+    
+  }
+    */
   tempControl();
+   mois = getMoisture();
+   hum  = getTemp();
+   temp = getHum();
+   
+   Serial.println("==========");
+   Serial.println("Boden: " + String(mois));
+   Serial.println("Luft: "+  String(hum));
+   Serial.println("Temp: "+  String(temp));
+   Serial.println("==========");
 
 }
 /*
@@ -99,12 +88,6 @@ float getHum() {
   return lastValues.humidity;
 }
 
-String getTempAndHum() {
-  String erg = "Temp: " + String(getTemp(), 0) + ",Hum: " + String(getHum(), 0);
-
-  return erg;
-
-}
 void activateFan() {
   digitalWrite(FAN_PIN, HIGH);
 }
@@ -120,8 +103,9 @@ void tempControl() {
     if (getTemp()> tempLimit) {
       activateFan();
 
+    }else{
+      deactivateFan();
     }
 
   }
 }
->>>>>>> Stashed changes
