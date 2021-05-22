b5b66202f88e8b3a1aabad85def0b48743a2b6b1
BeckiLukas <lukas-beckmann@gmx.net>
2021-05-22 03:24:25 +0200

    Add App

---
+++ VIGMini-App/app/src/main/java/ddns/net/vigmini/ForgotPasswordActivity.kt
@@ -0,0 +1,11 @@
+package ddns.net.vigmini
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ForgotPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+}
