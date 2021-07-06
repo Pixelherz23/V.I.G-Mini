a7067f721c44f0fdea8d2386754e2d3f8d2bab1b
BeckiLukas <lukas-beckmann@gmx.net>
2021-07-06 12:51:07 +0200

    last commit

--- VIGMini-App/app/src/main/java/ddns/net/vigmini/data/access/Constants.kt
+++ VIGMini-App/app/src/main/java/ddns/net/vigmini/data/access/Constants.kt
@@ -4,3 +4,7 @@ const val BASE_URL = "https://vig-mini.ddns.net/"
 const val SEEKBAR_TEMP_RANGE = 20
 const val DETAIL_ITEMS = 4
 const val PRODUCT_KEY = "productKey"
+const val TIMETABLE_LIGHT = "light"
+const val TIMETABLE_SOIL_MOISTURE = "soilMoisture"
+const val INFORMATION_NEWS = "news"
+const val INFORMATION_HELP = "help"
